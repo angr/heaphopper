de5e32a7321f9705da0ebfceea81abc8378f26c2
Moritz Eckert <mo@mightym0.de>
2018-07-16 19:38:02 +0200

    Build tests with poc-print by default

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -52,7 +52,7 @@ def create_poc_single(folder_name, analysis_name, binary_name, result_name, desc
 
     poc_path = glob.glob(poc_path)[0]
     try:
-        check_call(['make', '-C', poc_path], stdout=DEVNULL, stderr=STDOUT)
+        check_call(['make', '-C', poc_path, 'pocs-print'], stdout=DEVNULL, stderr=STDOUT)
     except CalledProcessError:
         return False
 
