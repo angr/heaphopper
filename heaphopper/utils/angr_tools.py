d420ab1d2361a375e4d48ec45d78385822d2ede9
Kevin Phoenix <kevin@kphoenix.us>
2021-10-18 13:14:13 -0700

    Handle stdin being None in utils.angr_tools.heardEnter

--- heaphopper/utils/angr_tools.py
+++ heaphopper/utils/angr_tools.py
@@ -5,11 +5,12 @@ from angr import SimPacketsStream
 
 
 def heardEnter():
-    i, o, e = select.select([sys.stdin], [], [], 0.0001)
-    for s in i:
-        if s == sys.stdin:
-            input = sys.stdin.readline()
-            return True
+    if sys.stdin is not None:
+        i, o, e = select.select([sys.stdin], [], [], 0.0001)
+        for s in i:
+            if s == sys.stdin:
+                sys.stdin.readline()
+                return True
     return False
 
 
